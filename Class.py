1b61d3c3341ce3dbcc48149c9f63a543c2264710
Cegepgvall <1536504@cegepoutaouais.qc.ca>
2023-05-15 09:14:14 -0400

    first save (gab)

--- Class.py
+++ Class.py
@@ -30,6 +30,27 @@ class Personne(object):
 
 class Employe(Personne):
     # Méthode d'access
+    def getcode(self):
+        return self.__code
+    def setcode(self, value):
+        self.__code = value
+
+    def getfonction(self):
+        return self.__fonction
+    def setfonction(self, value):
+        self.__fonction = value
+
+    # Constructeur
+    def __init__(self, setnom, setprenom, code: int = 0, fonction: str = ""):
+        super().__init__(setnom)
+        super().__init__(setprenom)
+        self.setcode(code)
+        self.setfonction(fonction)
+    # Méthode d'affichage
+        return f"{super().__str__(getnom)}\n{super().__str__(getprenom)}\nCode: {self.__getcode()}\nFonction: {self.getFonction()}\n"
+
+class Client(Personne):
+    # Méthode d'access
     def gettelephone(self):
         return self.__telephone
     def settelephone(self, value):
@@ -41,13 +62,58 @@ class Employe(Personne):
         self.__courriel = value
 
     # Constructeur
-    def __init__(self, nom, prenom, telephone: str = "", courriel: str = ""):
-        super().__init__(nom, prenom)
+    def __init__(self, setnom, setprenom, telephone: str = "", courriel: str = ""):
+        super().__init__(setnom)
+        super().__init__(setprenom)
         self.settelephone(telephone)
         self.setcourriel(courriel)
     # Méthode d'affichage
-        return f"{super().__str__()}, Telephone: {self.__gettelephone()}\nCourriel: {self.getcourriel()}\n"
+        return f"{super().__str__(getnom)}\n{super().__str__(getprenom)}\nTelephone: {self.__gettelephone()}\nCourriel: {self.getcourriel()}\n"
+
+class Reparation(object):
+    # Méthodes d'access
+    def getcode(self):
+        return self.__code
+    def setcode(self, value):
+        self.__code = value
+
+    def getdescription(self):
+        return self.__description
+    def setdescription(self, value):
+        self.__description = value
+
+    def getmontant(self):
+        return self.__montant
+    def setmontant(self, value):
+        self.__montant = value
+
+    def getdatereparation(self):
+        return self.__datereparation
+    def setdatereparation(self, value):
+        self.__datereparation = value
+
+    def getcodeemploye(self):
+        return self.__codeemploye
+    def setcodeemploye(self, value):
+        self.__codeemploye = value
+
+    # Constructeur
+    def __init__(self, code: int = 0, description: str = "", montant: float = 0, datereparation: str = "", codeemploye: int = 0):
+        self.setcode(code)
+        self.setdescription(description)
+        self.setmontant(montant)
+        self.setdatereparation(datereparation)
+        self.setcodeemploye(codeemploye)
+
+    # Méthodes d'affichage
+    def __str__(self)-> str:
+        return f"Code: {self.getcode()}\nDescription: {self.getdescription()}\nMontant: {self.getmontant()}\nDate: {self.getdatereparation()}\nCode Employer: {self.getcodeemploye()}\n"
+
+
 
 test1 = Personne("Gsdghsghfgh", "Gsfghfgdhfxg")
 print(test1)
-test2 = Employe(test1, "okk", "ok")
+#test2 = Employe("GAFafdsdfg", "Oksdfdg", "okk", "ok")
+#print(test2)
+test3 = Reparation(2, "ROUILLE", 10.54, "3 avril", 4)
+print(test3)
